5aa3c4f0b0d1beea4fe002ab55e1aba10b15ca09
Lale98 <78683721+Lale98@users.noreply.github.com>
2021-06-09 16:57:32 +0200

    music

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,13 +1,46 @@
 <template>
-  <div></div>
+  <div class="card">
+      <img :src="item.poster" :alt="item.title">
+      <h2> {{item.title}} </h2>
+      <p> {{item.author}} </p>
+      <p> {{item.year}} </p>
+  </div>
 </template>
 
 <script>
 export default {
-
+    props: ['item'],
 }
 </script>
 
 <style lang="scss" scoped>
+    @import '../style/variables';
 
+    .card {
+        height: 360px;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: calc((100% / 5) - 30px);
+        margin: 10px 15px;
+        background-color: $mainColor;
+
+        img {
+            margin: 25px 0;
+            width: 160px;
+            height: 160px;
+            
+        }
+
+        h2 {
+            text-transform: uppercase;
+            margin-bottom: 10px; 
+            color: white
+        }
+
+        p {
+            color: rgba(white, 0.4);
+        }
+    }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,26 +1,56 @@
 <template>
     <main>
-        <Card />
+        <div class="music" >
+            <Card v-for="component,index in components" :key="index" :item="component" />
+        </div>
     </main>
 </template>
 
 <script>
 import Card from './Card';
+import axios from 'axios';
 
 export default {
     name: 'Main',
     components: {
         Card
+    },
+    data: function () {
+        return {
+            apiUrl : 'https://flynn.boolean.careers/exercises/api/array/music',
+            components: []
+        }
+    },
+    created: function () {
+        axios
+            .get(this.apiUrl)
+            .then(
+                (response) => {
+                    console.log(this.components);
+                    this.components = response.data.response;
+                }
+            )
     }
 
 }
 </script>
 
 <style lang="scss" scoped>
-    @import '../style/variables.scss';
+    @import '../style/variables';
 
     main {
         height: 93vh;
         background-color: $backgroundColor;
+
+    }
+    .music {
+        margin: 0 auto;
+        max-width: 1170px;
+        height: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-wrap: wrap;
+        align-content: center;
     }
 </style>
